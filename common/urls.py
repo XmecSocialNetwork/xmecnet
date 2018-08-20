712ee8a6dc0ea93871637c66a0376595a5b0555a
Siva <sivagrenganathan+github@protonmail.com>
2018-08-20 12:38:14 +0530

    Remove unused URL's

--- common/urls.py
+++ common/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
 
-from .views import register, login,otp,trying,trying2,isloggedin,logout,search
+from .views import register, login, logout
 
 urlpatterns = [
     path('register', register, name='register'),
     path('login', login, name='login'),
-    path('otp', otp, name='otp'),
-    path('isloggedin', isloggedin, name='isloggedin'),
-    path('logout', logout, name='logout'),
-    path('search', search, name='search'),
-    path('trying', trying, name='trying'),
-    path('trying2', trying2, name='trying2')
+    path('logout', logout, name='logout')
 ]
