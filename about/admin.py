7bbfeec986bdcb85c578b21c30e2fca8219b6ac5
Siva <sivagrenganathan+github@protonmail.com>
2018-09-06 18:13:13 +0530

    Add admin page views

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile, Email, WorkExperience, Telegram, Education
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(WorkExperience)
+admin.site.register(Telegram)
+admin.site.register(Email)
+admin.site.register(Education)
